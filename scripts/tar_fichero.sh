ec649829a1d432ba5af99fda5d874f626885ff19
Nikolay <nikolemur@gmail.com>
2017-03-13 08:36:22 +0100

    updates

--- scripts/tar_fichero.sh
+++ scripts/tar_fichero.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 ### Script para sacar informacion de un fichero que esta en un tar sin descomprimirlo ###
-### En este caso, busca en el fichero centos6/EULA la cadena CentOS e imprime la primera linea ###
+### En este caso, busca en el fichero centos6/EULA la cadena CentOS e imprime la primera cadena de texto ###
 
 hola=$(tar xfO centos6.tar.gz centos6/EULA |grep CentOS |awk '{print $1}')
 
