a4c71603e5ad589904bc420d6b7a04d3208bd34f
root <root@wxp-15279.metromadrid.net>
2017-06-13 10:39:41 +0000

    update scripting

--- scripts/encrypt/destroy.sh
+++ scripts/encrypt/destroy.sh
@@ -1,4 +1,5 @@
 ### SCRIPT QUE COJE TODAS LAS PALABRAS DE UN FICHERO CREADO PREVIAMENTE MEDIANTE EL PROGRAMA PYTHON "WORDS.PY" Y TRATA DE DESENCRIPTAR UN FICHERO ###
+### AL FINAL NOS MUESTRA LAS POSIBLES OPCIONES, Y NOS ENCARGAMOS DE PROBAR LA BUENA ###
 
 #! /bin/bash
 
@@ -9,13 +10,17 @@ echo $LINE
 done)
 password=(${prueba[@]})
 for i in "${password[@]}"; do
-openssl aes-256-cbc -d -a -in *.enc -pass pass:$i -out niko
+openssl aes-256-cbc -d -a -in *.enc -pass pass:$i -out /root/pruebas/niko_$i
 pre=$(echo $?)
  if [[ ${pre} == "0" ]]; then
- echo "la buena es:: $i"
- exit
+# echo "la buena es:: $i"
+ echo "Posibles contras: $i" >> posibles.txt
  fi 
 done
+
+echo "FIN"
+cat posibles.txt
+rm -rf posibles.txt
 }
 
 main "$@"
--- scripts/encrypt/words.py
+++ scripts/encrypt/words.py
@@ -1,7 +1,7 @@
 ### PROGRAMA DE PYTHON QUE CREA TODAS LAS COMBINACIONES POSIBLES CON LAS LETRAS QUE INDIQUEMOS ###
-### EJECUTAR Y EXPORTAR RESULTADO A FICHERO "PASS.TXT PARA EL SCRIPT DESTROY.SH ###
+### EJECUTAR Y EXPORTAR RESULTADO A FICHERO "PASS.TXT PARA EL SCRIPT DESTROY.SH ### python words.py >> pass.txt
 
 import itertools
-res = itertools.product('abcdefghijkmlnopqrstuvwxyz', repeat=4) # 3 is the length of your result.
+res = itertools.product('abcdefghijkmlnopqrstuvwxyz', repeat=4) # 4 es la longitud que queremos que tenga pudiendo ser la que queramos.
 for i in res: 
     print ''.join(i)
