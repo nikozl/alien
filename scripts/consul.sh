da24f48da827aaba084e4c5501116a508bc70935
Nikolay <nikolemur@gmail.com>
2017-06-15 12:43:14 +0200

    Update consul.sh

--- scripts/consul.sh
+++ scripts/consul.sh
@@ -1,26 +1,32 @@
 #! /bin/bash
+
 #set -e
 #set -x
 
-datos=($(curl http://16.0.96.20:8500/v1/kv/pctce?keys 2>/dev/null |cut -d "["  --output-delimiter=" " -f 1- |cut -d "]"  --output-delimiter=" " -f 1- |cut -d ","  --output-delimiter=" " -f 1- |cut -d '"'  --output-delimiter=" " -f 1-))
-
+datos=( $(curl --silent http://16.0.96.20:8500/v1/kv/pctce?keys | awk 'FS="/" {print $2}' | uniq) )
 
 main () {
+echo "hostname,rebuild,diskless,fecha,version"
 for i in "${datos[@]}" ;
 do
-cambio=${i/\//\, }
-cambio2=${cambio/\//\, }
-hostname=${cambio2#* }
-hostname2=$(printf '%s\n' "${hostname%%,*}")
-rebuild=$(curl http://16.0.96.20:8500/v1/kv/pctce/"${hostname2}"/rebuild?raw 2>/dev/null)
-diskless=$(curl http://16.0.96.20:8500/v1/kv/pctce/"${hostname2}"/diskless?raw 2>/dev/null)
+hostname=$i
+rebuild=$(curl --silent http://16.0.96.20:8500/v1/kv/pctce/"${hostname}"/rebuild?raw)
+diskless=$(curl --silent http://16.0.96.20:8500/v1/kv/pctce/"${hostname}"/diskless?raw)
+fecha=$(curl --silent http://16.0.96.20:8500/v1/kv/pctce/"${hostname}"/fecha?raw)
+version=$(curl --silent http://16.0.96.20:8500/v1/kv/pctce/"${hostname}"/version?raw)
  if [[ $rebuild == "" ]]; then
  rebuild="false"
  fi
  if [[ $diskless == "" ]]; then
  diskless="false"
  fi
-echo "$hostname2,$rebuild,$diskless"
+ if [[ $fecha == "" ]]; then
+ fecha="error"
+ fi
+ if [[ $version == "" ]]; then
+ version="error"
+ fi
+echo "$hostname,$rebuild,$diskless,$fecha,$version"
 done
 }
 
