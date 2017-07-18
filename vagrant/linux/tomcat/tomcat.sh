3e958cdee8151c9382bf60a4b79ce84afbf52ef2
Nikolay <nikolemur@gmail.com>
2017-07-18 13:25:57 +0200

    updating tomcat

--- vagrant/linux/tomcat/tomcat.sh
+++ vagrant/linux/tomcat/tomcat.sh
@@ -16,7 +16,7 @@ alternatives --install /usr/bin/jar jar /opt/jdk1.8.0_131/bin/jar 2
 alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_131/bin/javac 2
 alternatives --set jar /opt/jdk1.8.0_131/bin/jar
 alternatives --set javac /opt/jdk1.8.0_131/bin/javac
-
+#EXPORTAMOS VARIABLES JAVA
 export JAVA_HOME=/opt/jdk1.8.0_131
 export JRE_HOME=/opt/jdk1.8.0_131/jre
 export PATH=$PATH:/opt/jdk1.8.0_131/bin:/opt/jdk1.8.0_131/jre/bin
@@ -25,9 +25,6 @@ cd /tmp
 wget http://www.us.apache.org/dist/tomcat/tomcat-7/v7.0.79/bin/apache-tomcat-7.0.79.tar.gz
 tar xzf apache-tomcat-7.0.79.tar.gz
 mv apache-tomcat-7.0.79 /usr/local/tomcat7
-cd /usr/local/tomcat7/
-./bin/startup.sh
-
 
 echo 'all_proxy="http://16.0.96.20:3128"' >> /etc/environment
 echo 'http_proxy="http://16.0.96.20:3128"' >> /etc/environment
@@ -37,5 +34,27 @@ echo "JAVA_HOME=/opt/jdk1.8.0_131" >> /etc/environment
 echo "JRE_HOME=/opt/jdk1.8.0_131/jre" >> /etc/environment
 echo "PATH=$PATH:/opt/jdk1.8.0_131/bin:/opt/jdk1.8.0_131/jre/bin" >> /etc/environment
 
-echo "Reinicio maquina"|wall
-reboot
+yum install httpd -y
+echo "LoadModule proxy_module /usr/lib64/httpd/modules/mod_proxy.so" >> /etc/httpd/conf/httpd.conf
+echo "LoadModule proxy_http_module /usr/lib64/httpd/modules/mod_proxy_http.so" >> /etc/httpd/conf/httpd.conf
+echo "include /etc/httpd/vhost.d/*.conf" >> /etc/httpd/conf/httpd.conf
+
+hostnamectl set-hostname tomcatserver.local
+mkdir -p /etc/httpd/vhost.d
+touch /etc/httpd/vhost.d/00-tomcat-niko.conf
+cat > /etc/httpd/vhost.d/00-tomcat-niko.conf << EOF
+<VirtualHost *:80>
+  Servername tomcatserver.local
+  ProxyPass / http://172.28.128.3:8080/
+  ProxyPassReverse / http://172.28.128.3:80
+  ProxyPreserveHost On
+  ProxyRequests Off
+</VirtualHost>
+EOF
+#DISABLE SELINUX 
+setenforce 0
+#sed -i 's/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux
+#reboot
+cd /usr/local/tomcat7
+systemctl start httpd
+./bin/catalina.sh start
