72fd70f4555e1b20186b94352335beeacf65cbaf
Nikolay <nikolemur@gmail.com>
2017-08-09 13:42:37 +0200

    update files

--- packer/script_ubuntu.sh
+++ packer/script_ubuntu.sh
@@ -5,6 +5,6 @@ export http_proxy="http://16.0.96.20:3128"
 export https_proxy="http://16.0.96.20:3128"
 export no_proxy="127.0.0.1,16.0.0.0/8,172.28.0.0/16"
 
-sudo apt-get update -y
-sudo apt-get install apache2 -y
-sudo mkdir /root/nikopacker
+apt-get update -y
+apt-get install apache2 -y
+mkdir /root/nikopacker
