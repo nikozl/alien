b03e116b2c8deb030ec13a9de2d8218017fd2474
Niko <nikolay.zlatarev@ibm.com>
2018-02-16 08:43:03 +0100

    cleaning passwords

--- scripts/bios.sh
+++ scripts/bios.sh
@@ -3,12 +3,12 @@
 #set -e
 #set -x
 
-ip=()
+ip=(list_of_ips)
 
 version() {
-version=$(/usr/bin/sshpass -p "mmtel" ssh -o StrictHostKeyChecking=no siv@$i ''dmesg | grep -i intel | awk '{print $5}' | head -1'')
+version=$(/usr/bin/sshpass -p "your_password_here" ssh -o StrictHostKeyChecking=no siv@$i ''dmesg | grep -i intel | awk '{print $5}' | head -1'')
 if [[ $version == "supported." ]] ; then 
- version=$(/usr/bin/sshpass -p "mmtel" ssh -o StrictHostKeyChecking=no siv@$i ''dmesg |grep -2 -i ACPI: |grep RSDT |awk '{print $4}' '')
+ version=$(/usr/bin/sshpass -p "your_password_here" ssh -o StrictHostKeyChecking=no siv@$i ''dmesg |grep -2 -i ACPI: |grep RSDT |awk '{print $4}' '')
 elif [[ $version == "D865PERL" ]] ; then
  version="RL86510A"
 elif [[ $version == "D845PESV" ]] ; then
@@ -19,7 +19,7 @@ fi
 }
 
 hostname() {
-hostname=$(/usr/bin/sshpass -p "mmtel" ssh -o StrictHostKeyChecking=no siv@$i 'hostname')
+hostname=$(/usr/bin/sshpass -p "your_password_here" ssh -o StrictHostKeyChecking=no siv@$i 'hostname')
 }
 
 pintar() {
